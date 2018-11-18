23590eff3bf9555036b39f2b9a621ac5ebbc14d8
shniu <niushaohan@digcredit.com>
2018-11-18 20:07:03 +0800

    handle only a connection per time

---
+++ src/main/java/info/chaintech/rpc/example/RpcServer.java
@@ -0,0 +1,80 @@
+package info.chaintech.rpc.example;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by shniu on 2018/11/18.
+ *
+ * Ref:
+ *   - https://stackoverflow.com/questions/5489915/java-datainputstream-read-operations-throwing-exceptions
+ */
+
+@Slf4j
+public class RpcServer {
+
+    private static int port = 8000;
+
+    public static void main(String[] args) {
+        log.info("Start server on {} ...", port);
+
+        RpcServer rpcServer = new RpcServer();
+        rpcServer.init();
+    }
+
+    public void init() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                log.info("Server wait to accept a connection");
+                Socket client = serverSocket.accept();
+                new ServerHandlerThread(client);
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    private class ServerHandlerThread implements Runnable {
+
+        private Socket socket;
+        ServerHandlerThread(Socket socket) {
+            this.socket = socket;
+            new Thread(this).start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                // read data
+                DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+                String clientInputString = inputStream.readUTF();
+                log.info("The client send content: {}", clientInputString);
+
+                // write to client
+                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+                outputStream.writeUTF("hello");
+
+                // close
+                outputStream.close();
+                inputStream.close();
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        socket = null;
+                        log.warn("Socket exception close: {}", e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/info/chaintech/rpc/utils/ModelMapper.java
@@ -0,0 +1,74 @@
+package info.chaintech.rpc.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shniu on 2018/11/18.
+ */
+
+@Slf4j
+public class ModelMapper {
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    public Object make(Class<?> targetType, String fromJson) {
+        try {
+            init();
+            return objectMapper.readValue(fromJson, targetType);
+        } catch (IOException e) {
+            log.error("JSON转化为对象类型 {} 时出错，json：{}", targetType, fromJson, e);
+        }
+        return null;
+    }
+
+    public List<?> makeList(Class<?> targetType, String fromJson) {
+        try {
+            init();
+            JavaType javaType = objectMapper.getTypeFactory().constructParametrizedType(ArrayList.class, List.class, targetType);
+            return objectMapper.readValue(fromJson, javaType);
+        } catch (IOException e) {
+            log.error("JSON转化为对象类型 List of {} 时出错，json：{}", targetType, fromJson, e);
+        }
+        return null;
+    }
+
+    private void init() {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public String writeString(Object targetObject) {
+
+        if (targetObject == null) {
+            return null;
+        }
+
+        String toJson = null;
+
+        try {
+            init();
+            toJson = objectMapper.writeValueAsString(targetObject);
+        } catch (JsonProcessingException e) {
+            log.error("将对象装换成JSON字符串出错", e);
+        }
+
+        return toJson;
+    }
+
+    public String prettify(Object obj) {
+        try {
+            init();
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            // swallow
+            log.warn(e.getMessage());
+        }
+        return "";
+    }
+}
