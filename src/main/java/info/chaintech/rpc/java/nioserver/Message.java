4adf8d3f711b011a46ff70747bd5463361d41c9e
shniu <niushaohan@digcredit.com>
2018-11-22 18:51:00 +0800

    selector

--- src/main/java/info/chaintech/rpc/java/nioserver/Message.java
+++ src/main/java/info/chaintech/rpc/java/nioserver/Message.java
@@ -3,10 +3,33 @@ package info.chaintech.rpc.java.nioserver;
 import java.nio.ByteBuffer;
 
 /**
+ * Message of inbound or outbound
+ * <p>
  * Created by Administrator on 2018/11/22 0022.
  */
 public class Message {
+
+    /**
+     * Message payload
+     */
+    private MessageBuffer messageBuffer;
+
     private long socketId;
+
+    public Object metaData;
+
+    public Message(MessageBuffer messageBuffer) {
+        this.messageBuffer = messageBuffer;
+    }
+
+    public int writeToMessage(byte[] httpResponseBytes) {
+        return 0;
+    }
+
+    public int writeToMessage(ByteBuffer byteBuffer) {
+
+        return 0;
+    }
 
     public void setSocketId(long socketId) {
         this.socketId = socketId;
@@ -16,11 +39,19 @@ public class Message {
         return socketId;
     }
 
-    public void writeToMessage(byte[] httpResponseBytes) {
-
+    public MessageBuffer getMessageBuffer() {
+        return messageBuffer;
     }
 
-    public void writeToMessage(ByteBuffer readByteBuffer) {
+    public void setMessageBuffer(MessageBuffer messageBuffer) {
+        this.messageBuffer = messageBuffer;
+    }
 
+    public Object getMetaData() {
+        return metaData;
+    }
+
+    public void setMetaData(Object metaData) {
+        this.metaData = metaData;
     }
 }
---
+++ src/main/java/info/chaintech/rpc/java/nioserver/MessageBufferTest.java
@@ -0,0 +1,27 @@
+package info.chaintech.rpc.java.nioserver;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * MessageBuffer test
+ * Created by Administrator on 2018/11/22 0022.
+ */
+public class MessageBufferTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void getMessage() throws Exception {
+
+    }
+
+}
