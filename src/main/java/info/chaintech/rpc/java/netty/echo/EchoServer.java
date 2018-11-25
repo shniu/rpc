04c9d64aef560446e211490843c5f8263fe63a6d
shniu <niushaohan@digcredit.com>
2018-11-25 21:36:15 +0800

    netty echo demo

---
+++ src/main/java/info/chaintech/rpc/java/netty/echo/EchoServer.java
@@ -0,0 +1,62 @@
+package info.chaintech.rpc.java.netty.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Netty echo server
+ *   1. Implement a server handler
+ *   2. Bootstrap the server
+ * <p>
+ * Created by shniu on 2018/11/25.
+ */
+
+@Slf4j
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        new EchoServer(9000).start();
+    }
+
+    private void start() {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind().sync();
+            log.info(EchoServer.class.getName() + " started and listen on " + future.channel().localAddress());
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            log.error("", e);
+        } finally {
+            try {
+                group.shutdownGracefully().sync();
+            } catch (InterruptedException e) {
+                log.error("", e);
+            }
+        }
+    }
+}
